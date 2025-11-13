39354fb5d430a5d8bfe4dd3645c4fa5dbc2447c6
ptcreborn <144877632+ptcreborn@users.noreply.github.com>
2025-11-14 07:41:12 +0800

    Update comment-editor.js

--- comment-editor.js
+++ comment-editor.js
@@ -1,164 +1,213 @@
-// November 14, 2025
+// November 11, 2025
 
 (async() => {
-    let url = window.location.href;
-    url = new URL(url).pathname;
+    let user_id;
+    let win_title = document.title;
 
-    await checkUrlInSPDB();
-
-    async function checkUrlInSPDB() {
-        await initFunctions(['supabase', 'FirebaseModule', 'moment']);
-        let {
-            data,
-            error
-        } = await supabase.from('bca-website-posts').select('id').eq('url', url);
-        if (error) {
-            window.alert(error.message);
-            return;
-        }
-        if (data.length == 0)
-            return;
-        const id = data[0].id;
-        let comments_data = await supabase.from('bca-comments').select('fb_id, user_id(id, email, created_at, gender, prof_img, username, rank_id(rank_name, rank_image))').eq('bca_posts', id).order('date', {
-            ascending: true
-        });
-        if (comments_data.error) {
-            window.alert("Error in comments_data: " + comments_data.error.message);
-            return;
-        }
-        const comment_count = document.querySelector('.all-comments');
-        comment_count.innerText = `${comments_data.data.length > 1 ? `${comments_data.data.length} comments`: `${comments_data.data.length} comment`}`
-        for (const comment of comments_data.data) {
-            let fb_data = await FirebaseModule.fetchJSON(`https://storehaccounts-comments-default-rtdb.firebaseio.com/bca_comments/${comment.fb_id}/content.json`);
-
-            if (fb_data.hasOwnProperty('val') || fb_data.hasOwnProperty('attachments'))
-                buildChildComment(comment, fb_data.val, fb_data.attachments, fb_data.reply_data);
-            else
-                buildChildComment(comment, fb_data, null, null); // This is the older version....
-        }
-
-        document.getElementById('parent_container_comment').innerHTML += `<button></button>`;
+    window.submit = async() => {
+        await commentSystem();
     }
 
-    function buildChildComment(sp_data, fb_data, attachments, fb_reply) {
-        // Needed variables...
-        // Username
-        // Account Creation Date
-        // Profile Image
-        // Rank Name
-        // Rank Image
-        // Comment Content
-        // Comment Date
+    await initFunctions(['FirebaseModule', 'supabase']);
+    await commentEditorSystem();
 
-        let img_html = '';
+    async function commentSystem() {
+        await createComment();
 
-        if (attachments) {
-            attachments.forEach(item => {
-                let img = document.createElement('img');
-                img.src = item;
-                img_html += img.outerHTML;
+        // functions //
+        async function createComment() {
+            userCommentingDisplay();
+            let result = await patchUserID();
+            if (!result)
+                return;
+            let comment_form = document.querySelector('#comment_form');
+            let content = document.querySelector('#comment_form textarea').value;
+            let comment_key = new Date().getTime();
+            let uploaded_imgs = document.querySelectorAll('#img_attachments img');
+            let imgs_html = '';
+            let reply_data = ''; // stored as base64 encoded html
+            let imgs = [];
+            uploaded_imgs.forEach(item => {
+                imgs.push(item.src);
+                imgs_html += item.outerHTML;
             });
-        }
-
-        let comment_reply_data = '';
-        if (fb_reply == null)
-            comment_reply_data = '';
-        else {
-            try {
-                comment_reply_data = JSON.parse(fb_reply);
-            } catch (e) {
-                comment_reply_data = fb_reply;
+            // Check for reply-data attribute from the comment-form element. Then store!
+            if (comment_form.hasAttribute('reply-data'))
+                reply_data = comment_form.getAttribute('reply-data');
+            // Creating comment record in the FBDB
+            await FirebaseModule.patch(`https://storehaccounts-comments-default-rtdb.firebaseio.com/bca_comments.json`, JSON.stringify({
+                [comment_key]: {
+                    content: {
+                        val: content,
+                        attachments: imgs,
+                        reply_data: reply_data
+                    },
+                    auth: user_id[0].toString()
+                }
+            }));
+            // Creating personal record of the comment for the user in FBDB
+            await FirebaseModule.patch(`https://storehaccounts-comments-default-rtdb.firebaseio.com/bca_users/${user_id[0]}.json`, JSON.stringify({
+                [new Date().getTime()]: {
+                    fb_id: comment_key
+                }
+            }));
+            // Creating a record in the SBDB
+            let { data, error } = await supabase.from('bca-comments').insert({
+                date: 'now()',
+                fb_id: comment_key,
+                user_id: user_id[0],
+                bca_posts: await createBCAUrlRecord()
+            });
+            if (error) {
+                window.alert(error.message);
+                return;
             }
+            // Success comment!
+            // Append latest comment to the parent comment container instead of reloading the page.
+            document.getElementById('parent_container_comment').innerHTML += `
+            <div class="comment-child" style="background: beige;">
+                <img style="width: 64px; height: 64px; object-fit: cover;" src="${document.querySelector('#comment_form button img').src}">
+                <div style="display:block;padding-left:5px;">
+                    <span>Your comment has been submitted.</span>
+                    <p style="background: white;border: 1px solid seagreen;padding: 0 5px;">${content}</p>
+                    <div style="margin: 10px;">${imgs_html}</div>
+                </div>
+            </div>`;
+
+            await restoreCommentForm();
         }
 
-        let html = `<div id=${sp_data.fb_id} class='comment-child'>
-		<div class='left-bar'>
-			<img class='profile'
-			src='${sp_data.user_id.prof_img}' />
-			<a href='https://battlecatsarchive.blogspot.com/p/profile-page.html?view=${sp_data.user_id.email}'><b username id=${sp_data.user_id.id}>${sp_data.user_id.username}</b></a>
-			<div class="achievements">
-			<span>${sp_data.user_id.rank_id.rank_name}</span><img class="icon" loading="lazy"
-				src="${sp_data.user_id.rank_id.rank_image}">
-			</div>
-			<div class="achievements">
-			<span>${moment(sp_data.user_id.created_at).fromNow()}</span><span>${sp_data.user_id.gender === "M" ? `🙍🏻‍♂️`: `👧`}</span>
-			</div>
-		</div>
-		<div class='right-bar'>				
-			${comment_reply_data.hasOwnProperty('comment_id') ? 
-					`<div style="background:beige;padding: 10px; border-radius: 10px; border: 2px solid wheat; margin: 10px 0;">${comment_reply_data.html}<span>${document.getElementById(comment_reply_data.comment_id).querySelector('[comment-data]').innerText}</span></div>`: comment_reply_data}
-			<div>
-				<span comment-data>${fb_data}</span>
-				<div style='display: block; text-align: right;'>
-    				<b class="time">${moment(sp_data.fb_id).fromNow()}</b>
-				</div>
-			</div>
-			<div class='attachments'>
-                ${img_html}
-            </div>
-			<button class='main-button button' onclick='replyFunc(this.parentNode.parentNode.id)'>Reply</button>
-		</div>
-		</div>
-        <div style='width: 100%;' id='comment-form-attached-${sp_data.fb_id}'></div>`;
+        async function createBCAUrlRecord() {
+            let url = new URL(window.location.href);
+            // supabase functions takes here
+            let key = await checkIfURLinSPDB();
+            if (!key) {
+                let { data, error } = await supabase.from('bca-website-posts').insert({
+                    date: 'now()',
+                    url: url.pathname
+                }).select('id');
+                if (error) {
+                    window.alert("Error in creating record: " + error.message);
+                    return;
+                }
 
-        document.getElementById('parent_container_comment').innerHTML += html;
+                key = data[0].id;
+            }
+            return key;
+        }
+
+        async function checkIfURLinSPDB() {
+            let url = new URL(window.location.href);
+            let { data, error } = await supabase.from('bca-website-posts').select('id').eq(
+                'url', url.pathname
+            );
+            if (error) {
+                window.alert("Error has been encountered! " + error.message);
+                return;
+            }
+            if (data.length > 0) return data[0].id;
+            else return;
+        }
+
+        async function patchUserID() {
+            // user_id will be filled with value from function getUserInfo()
+            if (!user_id) return;
+            // index 0: user_id
+            // index 1: user_email
+            await FirebaseModule.patch(`https://storehaccounts-comments-default-rtdb.firebaseio.com/bca_users.json`, JSON.stringify({
+                [user_id[0]]: new Date().getTime()
+            }));
+            return true;
+        }
     }
 
-    window.replyFunc = async function(id) {
-        await onReply(id);
+    async function commentEditorSystem() {
+        let user_info = await getUserInfo();
+
+        if(!user_info) return;
+
+        // Image Upload Function
+        document.getElementById('btn_uploadImage').addEventListener('click', inputEventTrigger);
+
+        async function getUserInfo() {
+            user_id = await checkIfUserOnline();
+            // index 0: user_id
+            // index 1: user_email
+
+            if (!user_id) return;
+
+            let { data, error } = await supabase.from('users').select('prof_img, username').eq('email', user_id[1]).single();
+
+            if (error) {
+                window.alert(error.message);
+                return;
+            }
+
+            const comment_username = document.querySelector('#comment_form span');
+            const comment_avatar = document.querySelector('#comment_form img');
+
+            comment_username.innerText = data.username;
+            comment_avatar.src = data.prof_img;
+
+            document.querySelector('#comment_form').classList.remove('inactive-form');
+            document.querySelector('#comment_form').classList.add('active-form');
+
+            return user_id;
+        }
+
+        async function checkIfUserOnline() {
+            let { data, error } = await supabase.auth.getSession();
+            if (error) {
+                window.alert("Error occured! " + error.message);
+                return;
+            }
+            if (!data.session) {
+                window.alert("You need to login first before commenting to the website.");
+                document.querySelector('#comment_form').classList.remove('inactive-form');
+                document.querySelector('#comment_form').classList.add('active-form');
+                document.getElementById('comment_form').innerHTML = `<a href='https://battlecatsarchive.blogspot.com/p/signin-to-bca.html' class="main-button button" style="width: 100%; margin: 5px;">Login First before commenting.
+</a>`;
+                document.getElementById('comment_form').setAttribute('login-status', 'failed');
+                return;
+            }
+            async function getUserIDFromSPDB(email) {
+                let { data, error } = await supabase.from('users').select('id').eq('email', email);
+                if (error) {
+                    window.alert("Error in getting user-email. " + error.message);
+                    return;
+                }
+                if (data.length == 0) {
+                    window.alert("Empty records returned! " + data);
+                    return;
+                }
+                return data[0].id;
+            }
+            let user_id = await getUserIDFromSPDB(data.session.user.email);
+            return [user_id, data.session.user.email];
+        }
     }
 
-    async function onReply(id) {
-        //append comment form editor in the id comment-form-attached
-        //change the comment form editor placeholder into replying to...
-        //gather all parameters...
+    function inputEventTrigger() {
+        document.querySelector('#file_attachments').click();
 
-        const comment_form = document.getElementById('comment_form');
-        const comment_id = document.getElementById(id);
-        const comment_element = document.getElementById('comment-form-attached-' + id);
-        const user_id = comment_id.querySelector('b').id;
-        const username = comment_id.querySelector('b').innerText;
-
-        comment_element.appendChild(comment_form);
-        await scrollToElemID(id);
-
-        if (comment_form.getAttribute('login-status') == "failed") return;
-
-        document.getElementById('reset-comment-form').style.display = 'block';
-        document.getElementById('cancel-reply-btn').style.display = 'flex';
-
-        comment_form.querySelector('textarea').placeholder = `Replying to @${username}`;
-
-        comment_form.setAttribute('reply-data', JSON.stringify({
-            comment_id: id,
-            html: `<div style="height: fit-content; margin-top: 5px;">
-				<button onclick='scrollToElemID(${id})'>
-				Replied to <b>@${username}</b></button>
-			</div>`
-        }));
-
-        // Notifications for the next step...
+        ImgurJS.uploadMultipleImgs('file_attachments', 'img_attachments', () => {
+            document.getElementById('btn_uploadImage').innerHTML = `<img src="https://png.pngtree.com/png-vector/20190508/ourmid/pngtree-upload-cloud-vector-icon-png-image_1027251.jpg">Uploading...`;
+            document.getElementById('btn_uploadImage').style = 'opacity: 0.7; pointer-events: none';
+            document.title = "Uploading image...";
+        }, () => {
+            document.getElementById('btn_uploadImage').innerHTML = `<img src="https://png.pngtree.com/png-vector/20190508/ourmid/pngtree-upload-cloud-vector-icon-png-image_1027251.jpg">Upload Image`;
+            document.getElementById('btn_uploadImage').style = 'opacity: 1; pointer-events: auto';
+            document.title = win_title;
+        }, () => console.log("Error!"));
     }
 
-    window.scrollToElemID = async function(id) {
-        await sleep(100);
-        document.getElementById(id).scrollIntoView({
-            behavior: 'auto',
-            block: 'center',
-            inline: 'center',
-        });
-        document.getElementById(id).style.background = `beige`;
-        await sleep(1500);
-        document.getElementById(id).style.background = `white`;
-    }
-
-    // MISC Functions	
     async function initFunctions(dependencies) {
         for (let i = 0; i < dependencies.length; i++)
             await waitFunctionsGetDefined(dependencies[i]);
     }
+
     async function waitFunctionsGetDefined(funcName) {
-        return new Promise(async (resolve) => {
+        return new Promise(async(resolve) => {
             while (true) {
                 try {
                     funcName = eval(funcName);
@@ -170,7 +219,44 @@
             }
         });
     }
-    async function sleep(ms) {
+
+    function sleep(ms) {
         return new Promise(resolve => setTimeout(resolve, ms));
+    }
+
+    function userCommentingDisplay() {
+        // this function will temporarily disable the form
+        // will change the title to commenting
+        // change the button text to commenting.
+        document.querySelector('#comment_form').style = 'pointer-events: none; opacity: 0.5;';
+    }
+
+    async function scrollToElemID(id) {
+        await sleep(100);
+        document.getElementById(id).scrollIntoView({
+            behavior: 'auto',
+            block: 'center',
+            inline: 'center',
+        });
+        document.getElementById(id).style.background = `beige`;
+        await sleep(1500);
+        document.getElementById(id).style.background = `white`;
+    }
+    async function restoreCommentForm() {
+        document.getElementById('btn_uploadImage').removeEventListener('click', inputEventTrigger);
+        document.getElementById('reset-comment-form').style.display = 'none';
+        document.querySelector('#comment_form textarea').placeholder = `Type comment here...`;
+        document.querySelector('#comment_form textarea').value = ``;
+        document.querySelector('#comment_form').style = 'pointer-events: auto; opacity: 1;';
+        document.getElementById('parent_container_comment').parentNode.insertBefore(document.querySelector('#comment_form'), document.getElementById('parent_container_comment').nextSibling);
+        document.getElementById('img_attachments').innerHTML = '';
+        document.querySelector('#comment_form').removeAttribute('reply-data');
+        document.getElementById('cancel-reply-btn').style.display = 'none';
+        document.getElementById('btn_uploadImage').addEventListener('click', inputEventTrigger);
+        await scrollToElemID('comment_form');
+    }
+
+    window.restoreComment = async() => {
+        await restoreCommentForm();
     }
 })();
