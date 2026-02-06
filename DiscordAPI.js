31a45b864302e03b50b9332bca68bcb2e6cb631c
ptcreborn <144877632+ptcreborn@users.noreply.github.com>
2026-02-06 19:56:56 +0800

    Create DiscordAPI.js

---
+++ DiscordAPI.js
@@ -0,0 +1,34 @@
+var DiscordAPI = {
+    post: async function(username, avatar, title, message, thumbnail, url) {
+        try {
+            const response = await fetch('https://discord.com/api/webhooks/1469281796368765192/dZEs_0S3SUSr3Lza8EHN3_glyLdU-o7KAyTVrvFbIWhXtYXuKFdaUHlElO6ZOlwoA3-X', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    username: username,
+                    avatar_url: avatar,
+                    "embeds": [{
+                        "title": title,
+                        "description": message,
+                        "color": 3447003,
+                        "image": {
+                            url: thumbnail
+                        },
+                        url: `${url == '' ? 'https://battlecatsarchive.blogspot.com/' : url}`
+                    }]
+                })
+            });
+
+            if (response.ok) { // Check if the response status is 200-299
+                console.log('Message sent successfully!');
+            } else {
+                const errorText = await response.text();
+                console.error(`Error sending message: ${response.status} - ${errorText}`);
+            }
+        } catch (error) {
+            console.error('Network or fetch error:', error);
+        }
+    }
+}
