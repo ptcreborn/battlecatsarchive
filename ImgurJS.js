25e615a6de3fa46d6ae1f3b23b3d67decc1eb94d
ptcreborn <144877632+ptcreborn@users.noreply.github.com>
2026-02-25 21:57:24 +0800

    Update ImgurJS.js

--- ImgurJS.js
+++ ImgurJS.js
@@ -22,7 +22,7 @@ var ImgurJS = {
                     temp = temp.split('.');
                     temp[0] += 'm';
 
-                    if(temp[1] == 'gif') 
+                    if (temp[1] == 'gif')
                         document.getElementById(imgID).src = url.href;
                     else {
                         temp = temp.join('.');
@@ -84,5 +84,28 @@ var ImgurJS = {
             xhr.setRequestHeader('Authorization', 'Client-ID 33f63d5902f27e5');
             xhr.send(fd);
         }, false);
+    },
+    uploadB64Img: function(file) {
+        return new Promise((resolve, reject) => {
+            var fd = new FormData();
+            fd.append("image", file); // Append the file
+            var xhr = new XMLHttpRequest(); // Create the XHR (Cross-Domain XHR FTW!!!) Thank you sooooo much imgur.com
+            xhr.open("POST", "https://api.imgur.com/3/image"); // Boooom!
+            xhr.onload = function() {
+                if (xhr.status == 200) {
+                    let data = JSON.parse(xhr.responseText).data;
+                    resolve(data);
+                } else {
+                    window.alert(`ImgurXHR error: Error in uploading... Please try again
+                        Status: ${xhr.status}
+                        Please try again!`);
+                    reject(`ImgurXHR error: Error in uploading... Please try again
+                        Status: ${xhr.status}
+                        Please try again!`);
+                }
+            }
+            xhr.setRequestHeader('Authorization', 'Client-ID 33f63d5902f27e5');
+            xhr.send(fd);
+        });
     }
 }
