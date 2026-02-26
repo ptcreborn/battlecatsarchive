abfd8811481d4f45c327cd8daeec11a4becda3f7
ptcreborn <144877632+ptcreborn@users.noreply.github.com>
2026-02-26 19:20:04 +0800

    Update sth_comment_builder.js

--- sth_comment_builder.js
+++ sth_comment_builder.js
@@ -1,4 +1,9 @@
 (async () => {
+
+	// Created by Lem for Storehaccounts community
+	// February 26, 2026
+	// Serves to build all the comments within the requested thread.
+	
 	await initFunctions(['moment', 'supabase']);
     await getComments();
 
