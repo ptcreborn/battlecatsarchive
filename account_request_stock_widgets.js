9df909802621fd6c31378a1acb2f20dcda52e415
ptcreborn <144877632+ptcreborn@users.noreply.github.com>
2026-03-02 02:52:46 +0800

    Create account_request_stock_widgets.js

---
+++ account_request_stock_widgets.js
@@ -0,0 +1,70 @@
+(async() => {
+    // Stock Widgets with Tabs!
+    // Made by Lemuel Madridejos March 2, 2026
+
+    window.show_tab = async(elem, div_id, toolbar_color) => {
+        const tab = document.getElementById(div_id);
+
+        document.querySelectorAll('#home_tab div[id]').forEach(item => {
+            item.classList.remove('tab_open');
+        });
+
+        document.querySelectorAll('#home_tab button').forEach(item => {
+            item.classList.remove('focused');
+        });
+
+        elem.classList.add('focused');
+        tab.classList.add('tab_open');
+        document.querySelector('#home_tab > div').className = `ui ${toolbar_color} message`;
+    }
+
+    // Listeners
+    document.getElementById('btn_tab_lowstock').addEventListener('click', await loadStockRequests());
+
+    // By default
+    show_tab(document.getElementById('btn_tab_recent'), 'acc_req_parent', 'violet');
+
+    async function loadStockRequests() {
+        if(document.getElementById('acc_req_lowstock').children.length > 0)
+            return;
+
+        await initFunctions(['FirebaseModule', 'moment']);
+        const parent = document.getElementById('acc_req_lowstock');
+        let data = await FirebaseModule.fetchJSON('https://storehaccounts-website-default-rtdb.firebaseio.com/lowkeep.json');
+        let keys = Object.keys(data);
+        keys = keys.reverse();
+
+        keys.forEach(key => {
+            let item = data[key];
+            parent.innerHTML += `<a href='${item.href}' style="                    
+                        min-height: 50px;
+                        border-bottom: 1px solid ${getRandomDarkColor()};
+                        display: flex;
+                        align-items: center;
+                    "><img loading="lazy" src="${item.img}" style="
+                        max-height: 40px;
+                        object-fit: cover;
+                        margin: 5px;
+                    "><div><span style="
+                        background: ${getRandomDarkColor()};
+                        color: white;
+                        padding: 0 5px;
+                        font-weight: 550;
+                    ">${item.username}</span> has reported...<br><p style='line-height: 1.2rem;font-size: 0.9rem;'>Please notice <b>[${item.acc_name} v${item.accver}]</b> — <b>${item.stock} account${item.stock > 1 ? `s`: ``} left.</b></p></div><span style="margin-left: auto; padding: 0 5px; font-weight: 550; line-height: 1rem;">${moment(parseInt(key)).fromNow()}</span></a>`;
+        });
+
+    }
+
+    async function loadNews() {
+
+    }
+
+    // MISC Functions
+    function getRandomDarkColor() {
+        const hue = Math.floor(Math.random() * 360); // any color
+        const saturation = 60 + Math.random() * 30; // 60–90%
+        const lightness = 20 + Math.random() * 25; // 20–45% (dark)
+
+        return `hsl(${hue}, ${saturation}%, ${lightness}%)`;
+    }
+})();
