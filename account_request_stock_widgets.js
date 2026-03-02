a5764daa03731d8dce3c1c6619d1a475c1491765
ptcreborn <144877632+ptcreborn@users.noreply.github.com>
2026-03-02 19:54:20 +0800

    Update account_request_stock_widgets.js

--- account_request_stock_widgets.js
+++ account_request_stock_widgets.js
@@ -1,8 +1,8 @@
-(async() => {
+(async () => {
     // Stock Widgets with Tabs!
     // Made by Lemuel Madridejos March 2, 2026
 
-    window.show_tab = async(elem, div_id, toolbar_color) => {
+    window.show_tab = async (elem, div_id, toolbar_color) => {
         const tab = document.getElementById(div_id);
 
         document.querySelectorAll('#home_tab div[id]').forEach(item => {
@@ -20,12 +20,13 @@
 
     // Listeners
     document.getElementById('btn_tab_lowstock').addEventListener('click', await loadStockRequests());
+    document.getElementById('btn_tab_restock').addEventListener('click', await loadNews());
 
     // By default
     show_tab(document.getElementById('btn_tab_recent'), 'acc_req_parent', 'violet');
 
     async function loadStockRequests() {
-        if(document.getElementById('acc_req_lowstock').children.length > 0)
+        if (document.getElementById('acc_req_lowstock').children.length > 0)
             return;
 
         await initFunctions(['FirebaseModule', 'moment']);
@@ -50,13 +51,39 @@
                         color: white;
                         padding: 0 5px;
                         font-weight: 550;
-                    ">${item.username}</span> has reported...<br><p style='line-height: 1.2rem;font-size: 0.9rem;'>Please notice <b>[${item.acc_name} v${item.accver}]</b> — <b>${item.stock} account${item.stock > 1 ? `s`: ``} left.</b></p></div><span style="margin-left: auto; padding: 0 5px; font-weight: 550; line-height: 1rem; font-size: 12px; opacity: 0.7;">${moment(parseInt(key)).fromNow()}</span></a>`;
+                    ">${item.username}</span> has reported...<br><p style='line-height: 1.2rem;font-size: 0.9rem;'>Please notice <b>[${item.acc_name} v${item.accver}]</b> — <b>${item.stock} account${item.stock > 1 ? `s` : ``} left.</b></p></div><span style="margin-left: auto; padding: 0 5px; font-weight: 550; line-height: 1rem; font-size: 12px; opacity: 0.7;">${moment(parseInt(key)).fromNow()}</span></a>`;
         });
 
     }
 
     async function loadNews() {
+        if (document.getElementById('acc_req_restock').children.length > 0)
+            return;
 
+        await initFunctions(['FirebaseModule', 'moment']);
+        const parent = document.getElementById('acc_req_restock');
+        let data = await FirebaseModule.fetchJSON('https://storehaccounts-website-default-rtdb.firebaseio.com/highkeep.json');
+        let keys = Object.keys(data);
+        keys = keys.reverse();
+
+        keys.forEach(key => {
+            let item = data[key];
+            parent.innerHTML += `<div style="                    
+                        min-height: 50px;
+                        border-bottom: 1px solid ${getRandomDarkColor()};
+                        display: flex;
+                        align-items: center;
+                    "><img src="${item.profile}" style="
+                        max-height: 40px;
+                        object-fit: cover;
+                        margin: 5px;
+                    "><div><span style="
+                        background: ${getRandomDarkColor()};
+                        color: white;
+                        padding: 0 5px;
+                        font-weight: 550;
+                    ">ADMIN: ${item.email}</span> has added to accounts...<br><p style='line-height: 1.2rem;font-size: 0.9rem;'>New uploads: <b>[${item.acc} v${item.ver}]</b> — has now <b>${item.qty} account${item.stock > 1 ? `s` : ``} more!</b></p></div><span style="margin-left: auto; padding: 0 5px; font-weight: 550; line-height: 1rem; font-size: 12px; opacity: 0.7;">${moment(parseInt(key)).fromNow()}</span></div>`;
+        });
     }
 
     // MISC Functions
