a8a3e2e58ea23f10ae0fb9cb56694e12b9efd92a
ptcreborn <144877632+ptcreborn@users.noreply.github.com>
2026-03-02 18:48:52 +0800

    Update account_request_stock_widgets.js

--- account_request_stock_widgets.js
+++ account_request_stock_widgets.js
@@ -50,7 +50,7 @@
                         color: white;
                         padding: 0 5px;
                         font-weight: 550;
-                    ">${item.username}</span> has reported...<br><p style='line-height: 1.2rem;font-size: 0.9rem;'>Please notice <b>[${item.acc_name} v${item.accver}]</b> — <b>${item.stock} account${item.stock > 1 ? `s`: ``} left.</b></p></div><span style="margin-left: auto; padding: 0 5px; font-weight: 550; line-height: 1rem;">${moment(parseInt(key)).fromNow()}</span></a>`;
+                    ">${item.username}</span> has reported...<br><p style='line-height: 1.2rem;font-size: 0.9rem;'>Please notice <b>[${item.acc_name} v${item.accver}]</b> — <b>${item.stock} account${item.stock > 1 ? `s`: ``} left.</b></p></div><span style="margin-left: auto; padding: 0 5px; font-weight: 550; line-height: 1rem; font-size: 12px; opacity: 0.7;">${moment(parseInt(key)).fromNow()}</span></a>`;
         });
 
     }
