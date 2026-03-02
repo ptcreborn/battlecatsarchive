037349312ad49b5da893a0638916d0ee2dd6ad48
ptcreborn <144877632+ptcreborn@users.noreply.github.com>
2026-03-02 20:03:40 +0800

    Update account_request_stock_widgets.js

--- account_request_stock_widgets.js
+++ account_request_stock_widgets.js
@@ -42,7 +42,7 @@
                         border-bottom: 1px solid ${getRandomDarkColor()};
                         display: flex;
                         align-items: center;
-                    "><img loading="lazy" src="${item.img}" style="
+                    "><img src="${item.img}" style="
                         max-height: 40px;
                         object-fit: cover;
                         margin: 5px;
@@ -82,7 +82,7 @@
                         color: white;
                         padding: 0 5px;
                         font-weight: 550;
-                    ">ADMIN: ${item.email}</span> has added to accounts...<br><p style='line-height: 1.2rem;font-size: 0.9rem;'>New uploads: <b>[${item.acc} v${item.ver}]</b> — has now <b>${item.qty} account${item.stock > 1 ? `s` : ``} more!</b></p></div><span style="margin-left: auto; padding: 0 5px; font-weight: 550; line-height: 1rem; font-size: 12px; opacity: 0.7;">${moment(parseInt(key)).fromNow()}</span></div>`;
+                    ">ADMIN: ${item.email}</span><br><p style='line-height: 1.2rem;font-size: 0.9rem;'>New uploads: <b>[${item.acc} v${item.ver}]</b></br>— Additional: <b>${item.qty} account${item.stock > 1 ? `s` : ``} more!</b></p></div><span style="margin-left: auto; padding: 0 5px; font-weight: 550; line-height: 1rem; font-size: 12px; opacity: 0.7;">${moment(parseInt(key)).fromNow()}</span></div>`;
         });
     }
 
