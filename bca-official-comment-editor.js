886ec0443684a8fb740c61a63d9a4525b6e22162
ptcreborn <144877632+ptcreborn@users.noreply.github.com>
2026-03-29 19:35:10 +0800

    Update bca-official-comment-editor.js

--- bca-official-comment-editor.js
+++ bca-official-comment-editor.js
@@ -732,6 +732,7 @@
             clone.querySelector('.bca-desc-rank').textContent = `Your comment has been added.`;
             clone.querySelector('.bca-desc-stats').remove();
             clone.querySelector('.bca-btn-reply').remove();
+            clone.querySelector('.bca-btn-replies').remove();
             clone.querySelector('.bca-btn-timeago').textContent = `${new Date()}`;
             clone.querySelector('.bca-desc-header').style.background = `#2c2c2c`;
             clone.querySelector('.bca-desc-text').textContent = editor.innerHTML;
