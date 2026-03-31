4e879da28b8f0e0854f2c6683253b12ebb49a526
ptcreborn <144877632+ptcreborn@users.noreply.github.com>
2026-03-31 16:34:11 +0800

    Update bca-comment-main.js

--- bca-comment-main.js
+++ bca-comment-main.js
@@ -100,7 +100,7 @@
     function triggerCommentViewer() {
         setTimeout(() => {
             appendCSSFile('https://rawcdn.githack.com/ptcreborn/battlecatsarchive/fe2291a6666ee8e2b09714e1d3bd5ef4d42fbfdd/bca-comment-viewer.css');
-            appendJSFile('https://rawcdn.githack.com/ptcreborn/battlecatsarchive/83051ebcd7971a8ce865e51d4835acdfc2eff052/bca-official-comment-viewer.js');
+            appendJSFile('https://rawcdn.githack.com/ptcreborn/battlecatsarchive/a10c05fd77dc73d9ddda5f8749d9d6b9c6a72688/bca-official-comment-viewer.js');
         }, 1);
     }
 })();
