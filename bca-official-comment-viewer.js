8a7818eb5da4d7303f37be8d3c2af3040598b6f0
ptcreborn <144877632+ptcreborn@users.noreply.github.com>
2026-03-31 16:36:03 +0800

    Update bca-official-comment-viewer.js

--- bca-official-comment-viewer.js
+++ bca-official-comment-viewer.js
@@ -31,7 +31,7 @@
 
         hasMoreComments = total_page > 1;
 
-        if (total_page == 0) {
+        if (!total_page || total_page == 0) {
             trigger_btn.style.opacity = '0.7';
             trigger_btn.textContent = "You've reached the end.";
             return;
