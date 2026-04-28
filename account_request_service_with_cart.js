53e520b399ce3ee47c78bd9ea84cffc4df7242e3
ptcreborn <144877632+ptcreborn@users.noreply.github.com>
2026-04-28 23:50:48 +0800

    Update account_request_service_with_cart.js

--- account_request_service_with_cart.js
+++ account_request_service_with_cart.js
@@ -547,7 +547,7 @@
 
                 queryP(clone, 'store-account-sold').textContent = `${item.request_count} sold`;
                 queryP(clone, 'store-account-stats').textContent = `${await getTotalRequester(item.id)} users like this...`;
-                queryP(clone, 'store-account-info').querySelectorAll('p')[0].textContent = `${item.description}`; // cat food
+                queryP(clone, 'store-account-info').querySelectorAll('p')[0].textContent = `${item.description.replaceAll('\n', '<br/>')}`; // cat food
 
                 // queryP(clone, 'store-account-info').querySelectorAll('p')[1].textContent = `${item.description.split('\n')[1]}`; // XP
 
