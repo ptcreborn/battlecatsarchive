6ad53ec2de4ce2526fa75a0690bac642afcc6042
ptcreborn <144877632+ptcreborn@users.noreply.github.com>
2026-04-29 16:47:03 +0800

    Update account_request_stock_widgets.js

--- account_request_stock_widgets.js
+++ account_request_stock_widgets.js
@@ -37,9 +37,11 @@
 
         keys.forEach(key => {
             let item = data[key];
+
+            // border-bottom: 1px solid ${getRandomDarkColor()};
+            // background: ${getRandomDarkColor()};
             parent.innerHTML += `<a href='${item.href}' style="                    
                         min-height: 50px;
-                        border-bottom: 1px solid ${getRandomDarkColor()};
                         display: flex;
                         align-items: center;
                     "><img src="${item.img}" alt='${item.username} requests low-stock' loading='lazy' style="
@@ -48,7 +50,6 @@
                         object-fit: cover;
                         margin: 5px;
                     "><div><span style="
-                        background: ${getRandomDarkColor()};
                         color: white;
                         padding: 0 5px;
                         font-weight: 550;
@@ -82,7 +83,7 @@
                         color: #c9c9c9;
                         padding: 0 5px;
                         font-weight: 550;
-                    ">${item.email}</a><br><p style='line-height: 1.2rem;font-size: 0.9rem;'>${item.qty == 0 ? `Added a new Version!`: `New uploads: `}<b>[${item.acc} v${item.ver}]</b></br>— ${item.qty == 0 ? `New version opened: `: `Additional: `} <b>${item.qty} account${item.stock > 1 ? `s` : ``} more!</b></p></div><span style="margin-left: auto; padding: 0 5px; font-weight: 550; line-height: 1rem; font-size: 12px; opacity: 0.7;">${moment(parseInt(key)).fromNow()}</span></div>`;
+                    ">${item.email}</a><br><p style='line-height: 1.2rem;font-size: 0.9rem;'>${item.qty == 0 ? `Added a new Version!` : `New uploads: `}<b>[${item.acc} v${item.ver}]</b></br>— ${item.qty == 0 ? `New version opened: ` : `Additional: `} <b>${item.qty} account${item.stock > 1 ? `s` : ``} more!</b></p></div><span style="margin-left: auto; padding: 0 5px; font-weight: 550; line-height: 1rem; font-size: 12px; opacity: 0.7;">${moment(parseInt(key)).fromNow()}</span></div>`;
         });
     }
 
