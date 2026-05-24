6d97d0f86382f5b0018c95813b208619648f20bd
ptcreborn <144877632+ptcreborn@users.noreply.github.com>
2026-05-24 23:10:42 +0800

    Update page_bca_link_terminal_with_bypass.js

--- page_bca_link_terminal_with_bypass.js
+++ page_bca_link_terminal_with_bypass.js
@@ -484,6 +484,7 @@
             // bca_link_ads.style.transform = 'translate(-50%, -150px)';
             // bca_link_ads.style.left = '50%';
             bca_link_ads.style.margin = "0 auto";
+            bca_link_ads.style.top = "-150px";
             bca_link_ads.style.position = "relative";
             bca_link_ads.style.width = '';
 
