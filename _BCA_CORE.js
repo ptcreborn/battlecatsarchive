ed32601ca72d47ee9cc555e1e971e31ab7207ab5
ptcreborn <144877632+ptcreborn@users.noreply.github.com>
2026-06-03 00:52:48 +0800

    Update _BCA_CORE.js

--- _BCA_CORE.js
+++ _BCA_CORE.js
@@ -1,7 +1,7 @@
 
 appendJSFile('https://cdn.jsdelivr.net/npm/moment@2.30.1/moment.min.js');
 appendJSFile('https://rawcdn.githack.com/ptcreborn/storehaccounts/93f717900b4c70ddfee58d8ff9a89d323493ed61/FirebaseModule.js');
-appendCSSFile('https://rawcdn.githack.com/ptcreborn/battlecatsarchive/99dff01dc93fe7c3a64113292b23867394b4cab7/notification.css');
+appendCSSFile('https://rawcdn.githack.com/ptcreborn/battlecatsarchive/0f2b95e151c86d4b2387c5b96489b36328b26bbf/notification.css');
 
 var BCA_Notifications = {
     db: `https://ptc-notifications-default-rtdb.firebaseio.com/notifications`,
@@ -222,6 +222,7 @@ var BCA_Notifications = {
             const link = e.target.closest('.bca-notif-child');
 
             if (link?.dataset?.status == "unread") {
+                document.getElementById('bca-notif-content').style.pointerEvents = 'none';
                 await this.markRead(link.dataset.fbid, atob(encoded_email));
                 BCA_Cache.deleteItem(this.LOCALSTORAGE_NOTIF);
             }
