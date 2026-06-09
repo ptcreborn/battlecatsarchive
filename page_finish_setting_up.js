669c998e7218016e02d52c7431e795011be8faed
ptcreborn <144877632+ptcreborn@users.noreply.github.com>
2026-06-09 23:25:52 +0800

    Update page_finish_setting_up.js

--- page_finish_setting_up.js
+++ page_finish_setting_up.js
@@ -138,7 +138,7 @@
     async function checkUserCountry() {
         let res = await fetch('https://ipapi.co/json/');
         if (res.ok) {
-            country_data = await country_data.json();
+            let country_data = await country_data.json();
             if (!country_data.country_code) {
                 country_name = "Anonymous";
                 country_code = "Anonymous";
