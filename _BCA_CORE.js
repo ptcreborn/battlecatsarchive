48d681d260b340938a3b983811f311c37b8fe44b
ptcreborn <144877632+ptcreborn@users.noreply.github.com>
2026-06-29 15:16:41 +0800

    Update _BCA_CORE.js

--- _BCA_CORE.js
+++ _BCA_CORE.js
@@ -1112,7 +1112,7 @@ var BCA_Comment = {
     async buildCommentContents(parent, data) {
         let comment = await this.getFBCommentData(data.fb_id, 'content');
         let attach = await this.getFBCommentData(data.fb_id, 'attach');
-        this.wQuery(this.query(`bca-comments-${data.id}`), 'bca-message').textContent = `${comment}`;
+        this.wQuery(this.query(`bca-comments-${data.id}`), 'bca-message').textContent = `${comment.content?.val ? comment.content.val : comment}`;
         this.wQuery(this.query(`bca-comments-${data.id}`), 'bca-timestamp').textContent = `${moment(data.date).fromNow()}`;
 
         if (attach)
