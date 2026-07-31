537868e5ab5575b3ce14d669bfb3ec13cd24aadf
ptcreborn <144877632+ptcreborn@users.noreply.github.com>
2026-07-31 23:13:23 +0800

    Update bca_homepage_widget.js

--- bca_homepage_widget.js
+++ bca_homepage_widget.js
@@ -82,7 +82,7 @@
         return {
             isLessThanADay: timeDiff < ONE_DAY_MS,
             isLessThanAWeek: timeDiff < ONE_WEEK_MS,
-            isLessThanAMonth: timeDiff < ONE_MONTH_MS && !isLessThanADay && !isLessThanADay,
+            isLessThanAMonth: timeDiff < ONE_MONTH_MS && timeDiff > ONE_WEEK_MS,
             isGreaterThanAWeek: ONE_WEEK_MS >= timeDiff && timeDiff < ONE_MONTH_MS,
             isGreaterThanAMonth: timeDiff >= ONE_MONTH_MS,
             timeDiffMs: timeDiff
