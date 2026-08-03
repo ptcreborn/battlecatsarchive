194ed067e91740b05116255c0b7977049983132a
ptcreborn <144877632+ptcreborn@users.noreply.github.com>
2026-08-04 01:07:51 +0800

    Update _BCA_CORE.js

--- _BCA_CORE.js
+++ _BCA_CORE.js
@@ -1184,7 +1184,7 @@ var BCA_Comment = {
         let id = BCA_Url.getParamValue('target_comment')?.replace('bca-comments-', '');
         let target_comment_data = await this.getSingleCommentData(id);
 
-        console.log("First: " + target_comment_data);
+        console.log(target_comment_data);
 
         if (!target_comment_data)
             return;
