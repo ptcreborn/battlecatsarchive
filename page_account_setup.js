7e7a3939317259cd63fdccc993ec4df3559b91b3
ptcreborn <144877632+ptcreborn@users.noreply.github.com>
2026-08-16 00:12:40 +0800

    Update page_account_setup.js

--- page_account_setup.js
+++ page_account_setup.js
@@ -1,16 +1,14 @@
-(async() => {
+(async () => {
     await buildInformation();
 
     // Functions \\
     function getURLParameters(key) {
         let url = new URL(window.location.href);
-        let param = url.searchParams;
-        param = param.get(key);
+        let is_using_hash = checkHashCodeParam(key) || url.hash;
+        let param = is_using_hash ? getHashCodeParam(key) : url.searchParams.get(key);
 
-        if (!param) {
-            window.alert("No request parameter in the url.");
+        if (!param) 
             return;
-        }
 
         return param;
     }
@@ -19,7 +17,10 @@
         let data_param = getURLParameters('reserved');
         let acc_param = getURLParameters('verified');
 
-        if(!data_param || !acc_param)
+        console.log(data_param);
+        console.log(acc_param);
+
+        if (!data_param || !acc_param)
             return;
 
         let data = decodeURIComponent(data_param);
@@ -34,7 +35,7 @@
             return;
         }
 
-        if(data.account.email != acc) {
+        if (data.account.email != acc) {
             window.alert("Verification failed. Incorrect account.");
             return;
         }
@@ -44,13 +45,13 @@
 
     async function buildInformation() {
         let data = await getAccountInfo();
-        if(!data)
-            return;        
+        if (!data)
+            return;
 
         await initFunctions(['FirebaseModule']);
         let fb_data = await FirebaseModule.fetchJSON(`https://storehaccounts-website-default-rtdb.firebaseio.com/accounts_heap/${data.account.code}.json`);
 
-        if(!fb_data) {
+        if (!fb_data) {
             window.alert("Cant find what you're requesting to. The session might has been expired.");
             return;
         }
@@ -71,7 +72,35 @@
 
         btn.style.display = 'block';
         btn.addEventListener('click', () => {
-            window.location.href = `https://battlecatsarchive.blogspot.com/p/account-progress.html?ongoing=${getURLParameters('reserved')}&verified=${encodeURIComponent(btoa(fb_data.name))}`;
+            window.location.href = `https://battlecatsarchive.blogspot.com/p/account-progress.html#ongoing=${getURLParameters('reserved')}&verified=${encodeURIComponent(btoa(fb_data.name))}`;
         });
+    }
+
+    function checkHashCodeParam(param) {
+        let hash = window.location.hash.substring(1);
+
+        if (!hash)
+            return;
+
+        let hash_parts = hash.split('=');
+
+        return hash_parts.includes(`${param}`) || null;
+    }
+
+    function getHashCodeParam(param) {
+        let hash = window.location.hash.substring(1);
+
+        if (!hash)
+            return;
+
+        let hash_parts = hash.split('&');
+
+        let value = hash_parts.map(item => {
+            let item_parts = item.split(/=(.*)/s).filter(_ => _);
+            if (param === item_parts[0])
+                return item_parts[1];
+        }).filter(_ => _);
+
+        return value ? decodeURIComponent(value) : null;
     }
 })();
